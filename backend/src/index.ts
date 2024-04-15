5768579361245ae872c7207440776895ce3bba72
Ashish Bisht <ashish12345bisht@gmail.com>
2024-04-15 23:51:16 +0530

    envs remove

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,6 +15,4 @@ const app = new Hono<{
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
 
-// postgresql://practice_owner:ytr16GzIUdOX@ep-shrill-hall-a1jxdsyw.ap-southeast-1.aws.neon.tech/practice?sslmode=require
-// DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiNmE5MDhlMTItYWU1MS00N2MwLTk4YTgtNWIzZmM5MjM3MmEzIiwidGVuYW50X2lkIjoiMzkyMmIxYjViMjZjNTFmMDFhMjgxYWRmNTIzMTkzMGY1ZmQxMWQzMjhmZWRjZGMxMTYxNDc3NTUyM2Y0MjRjMSIsImludGVybmFsX3NlY3JldCI6IjNlODNkOWJjLTFkMDktNDUzMi05NDdjLWQ1YzdlNmNjOTMyNSJ9._dinokw4YMDx9oJME6c7DapX4EL_saNqc4JH_DP-tFw"
 export default app;
